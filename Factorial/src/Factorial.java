be2dfa51cf6b88da6a2339825f6513c5bd0a98c3
kvnikhil22 <89282292+kvnikhil22@users.noreply.github.com>
2022-02-21 15:00:52 -0600

    updated the class test

---
+++ Factorial/src/Factorial.java
@@ -0,0 +1,25 @@
+import java.util.*;
+/**
+ * Author : Nikhil KrishnanVenkatesh (S545019)
+ */
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = scanner.nextInt();
+        long factorial = fact(num);
+        System.out.println("Factorial of given number is: "+factorial);
+        for(int i=num; i>=0; i--) {
+            long a = fact(i);
+            System.out.println("F(" + i + ") = " + a);
+        }
+    }
+    public static long fact(int num)
+    {
+        if (num >= 1) {
+            return num * fact(num - 1);
+        }
+        else
+            return 1;
+    }
+}
